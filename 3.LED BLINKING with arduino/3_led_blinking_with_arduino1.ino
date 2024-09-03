515b4c45b018a3aa87d1e29cf9ccfb63f9b490d3
ravijrs <ravijrs789@gmail.com>
2024-09-03 12:37:08 +0530

    3.LED BLINKING with arduino

---
+++ 3.LED BLINKING with arduino/3_led_blinking_with_arduino1.ino
@@ -0,0 +1,17 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(11,HIGH);
+  digitalWrite(12,HIGH);
+  delay(1000); 
+  digitalWrite(11,LOW);
+  digitalWrite(12,LOW);
+  delay(1000);
+}
